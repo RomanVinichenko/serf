add6b072ca74ee656951a698a157b9daf8a600bc
RomanVinichenko <chicagezzz@gmail.com>
2021-05-06 23:29:56 +0300

    Edit

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-$(function () {});
+$(function () {
+
+  $('.blog__slider-items').slick({
+    dots: true,
+    arrows: false,
+  });
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+
+  });
+
+  var mixer = mixitup('.gallery__items');
+});
