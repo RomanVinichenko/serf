02773e3ae1f5f0774412eefde7f4e2050feee0a1
RomanVinichenko <chicagezzz@gmail.com>
2021-05-11 16:46:55 +0300

    changes

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,36 @@
 $(function () {
+    $('.menu a, .footer a').on('click', function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate(
+            {
+                scrollTop: top,
+            },
+            1500,
+        );
+    });
 
-  $('.menu a, .footer a').on('click', function (event) {
-    event.preventDefault();
-    var id = $(this).attr('href'),
-      top = $(id).offset().top;
-    $('body,html').animate({
-      scrollTop: top,
-    }, 1500);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('.header').addClass('header--active');
+        } else {
+            $('.header').removeClass('header--active');
+        }
+    });
 
-  });
+    $('.blog__slider-items').slick({
+        dots: true,
+        arrows: false,
+    });
 
-  $(window).scroll(function () {
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active');
+    });
 
-    if ($(this).scrollTop() > 150) {
-      $('.header').addClass('header--active');
-    } else {
-      $('.header').removeClass('header--active');
-    }
-  });
+    $('.menu__list-link, .logo').on('click', function () {
+        $('.menu__list--active').removeClass('menu__list--active');
+    });
 
-  $('.blog__slider-items').slick({
-    dots: true,
-    arrows: false,
-  });
-
-  $('.menu__btn').on('click', function () {
-    $('.menu__list').toggleClass('menu__list--active');
-
-  });
-
-  var mixer = mixitup('.gallery__items');
+    var mixer = mixitup('.gallery__items');
 });
